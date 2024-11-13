9f7f6421904ccf42cf3e81ee5e9e32b0872ae765
Jack Brosnan <ik7pdsoq@students.codeinstitute.net>
2024-11-13 15:19:13 +0000

    Deploying to Heruku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4j4j_7w-rp^sr57o60=q6ptlg8ph4&!x1a!8^6t_^-fyw$b836'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-jackbrosnan-djangoproje-s35za817osi.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-jackbrosnan-djangoproje-s35za817osi.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
